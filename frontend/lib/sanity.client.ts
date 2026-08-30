84e1867ef83ced7a5d8ecc91f68ae428f50e02b0
ZenDin082 <nidnezz41@gmail.com>
2026-08-30 14:29:40 +0700

    Revert "feat: setup daily reflection scenario UI and sanity integration"

--- frontend/lib/sanity.client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@sanity/client";
-
-export const client = createClient({
-  projectId: "v8udsf47",
-  dataset: "production",
-  apiVersion: "2024-01-01",
-  useCdn: true,
-});
